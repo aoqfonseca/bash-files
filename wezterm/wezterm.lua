6736a7cdadc16352ef6a4fc11b059e14b0150657
Andre Fonseca <andre.fonseca@carta.com>
2026-05-14 18:21:35 -0300

    Reorg folder and update the dotfiles

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,83 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.adjust_window_size_when_changing_font_size = false
+config.enable_tab_bar = true
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+-- Set Zsh as the default program and run it as a login shell
+config.default_prog = { "/bin/zsh", "-l" }
+
+-- Generic configs
+config.front_end = "WebGpu"
+
+-- Color
+config.color_scheme = "Catppuccin Mocha"
+config.window_background_opacity = 0.9
+
+--Font config
+--config.font = wezterm.font("0xProto Nerd Font")
+config.font = wezterm.font("IosevkaTerm Nerd Font")
+--config.font = wezterm.font("Inconsolata Nerd Font")
+config.font_size = 16
+config.window_decorations = "RESIZE"
+
+--Config Keys
+config.keys = {
+	{
+		key = "k",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ScrollToPrompt(-1),
+	},
+	{
+		key = "j",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ScrollToPrompt(1),
+	},
+
+	{
+		key = "q",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+	{
+		key = "Delete",
+		mods = "CTRL",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
+}
+
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
+wezterm.on("update-status", function(window, pane)
+	-- Get the effective configuration
+	local config = window:effective_config()
+
+	-- Extract font details
+	local font_family = config.font.font[1].family
+	local font_size = config.font_size
+
+	-- Format the status string
+	local font_status = string.format(" Font: %s, size [%s]   ", font_family, font_size)
+
+	-- Set the right status
+	window:set_right_status(wezterm.format({
+		{ Background = { Color = "white" } },
+		{ Foreground = { Color = "blue" } },
+		{ Text = font_status },
+	}))
+end)
+
+local tmux_session = require("plugins/tmux-session")
+tmux_session.apply_to_config(config)
+
+local font_selector = require("plugins/font-selector")
+font_selector.apply_to_config(config)
+
+return config
